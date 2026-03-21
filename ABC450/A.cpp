914a794636e5a666d14b221cc91cc562e5385734
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-21 18:53:14 +0600

    printing by reverse.

---
+++ ABC450/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = n; i >= 1; i--)
+    {
+        cout << i << ",";
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
