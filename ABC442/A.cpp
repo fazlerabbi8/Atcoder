fb41f03a516e081eb70f2825b6c1a4894b4f6326
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-01 15:21:02 +0600

    implementation of an equation.

---
+++ ABC442/A.cpp
@@ -0,0 +1,28 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve()
+// {
+//     string s;
+//     cin >> s;
+
+//     int count = 0;
+//     bool end = false;
+//     for(int i = 0, n = s.length(); i < n; i++){
+//         if(s[i] == 'i' || s[i] == 'j'){
+//             count++;
+//             end = true;
+//         }
+//     }
+//     if(end){
+//         cout << count << "\n";
+//     }else{
+//         cout << 0 << "\n";
+//     }
+// }
+// int main()
+// {
+//     solve();
+
+//     return 0;
+// }
