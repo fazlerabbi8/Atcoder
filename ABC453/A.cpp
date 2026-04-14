5a351950eb682bfacc1fb71572c996fac27f469b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-14 18:39:47 +0600

    leading zero.

---
+++ ABC453/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    while(s.size() > 0 && s[0] == 'o')
+    {
+        s.erase(s.begin());
+    }
+    cout << s << "\n";
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
